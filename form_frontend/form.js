44073489d7add332bee3b91f87cb4fe3b921e751
amanagn1hotr1 <aman@conscent.ai>
2024-04-23 17:04:59 +0530

    added project files

---
+++ form_frontend/form.js
@@ -0,0 +1,50 @@
+window.addEventListener("DOMContentLoaded",async()=>
+{
+ const getFirstQuestions=await fetch('http://localhost:5000/question',{method:"GET"});
+ const data=await getFirstQuestions.json();
+ const additionalQuestions=document.getElementById('additionalQuestions');
+ additionalQuestions.innerHTML=`<div>${data.question}</div>
+ ${data.options.map((option)=>(`<input type="radio" name="ageInput" class="s1" value=${option.value}>
+<label>${option.value}</label>`)).join('')}`;
+const radioInputs = document.getElementsByClassName('s1');
+for(let item of radioInputs){
+    console.log(item);
+    item.addEventListener("change",async(e) => {
+        const selectedValue = item.value;
+        prevValue=selectedValue;
+    })
+};
+})
+const fetchQuestion=async(value)=>
+{
+ console.log(value);
+ const data=await fetch(`http://localhost:5000/question/${value}`,{method:"GET"});
+ const currQuestion=await data.json();
+additionalQuestions.innerHTML=`<div> ${currQuestion.question}
+${currQuestion.options.map((option)=>(`<input type="radio" name="ageInput" class="s1" value=${option.value}>
+<label>${option.value}</label>`)).join('')
+}
+ </div>`;
+ const radioInputs = document.getElementsByClassName('s1');
+ for(let item of radioInputs){
+     console.log(item);
+     item.addEventListener("change",async(e) => {
+         const selectedValue = item.value;
+         console.log(selectedValue);
+         prevValue=selectedValue;
+     })
+ };
+}
+
+const nextButton=document.getElementById("nextButton");
+nextButton.addEventListener("click",async()=>
+{
+  await fetchQuestion(prevValue);
+})
+const submitBtn=document.getElementById('submitBtn');
+submitBtn.addEventListener("click",async()=>
+{  
+    const data=await fetch('http://localhost:5000/submitdata',{method:'GET',
+    headers:{ "Content-Type": "application/json"}});
+    console.log(await data.json());
+});
---
+++ index.js
@@ -0,0 +1,150 @@
+const express=require("express");
+const cors=require('cors');
+const app=express();
+const pg=require("pg");
+const PORT=5000;
+let data=[];
+app.use(express.json());
+app.use(cors());
+const { Client } = pg;
+const clientConfig = new Client({
+  host: 'localhost',
+  port: 5432,
+  database: 'postgres',
+  user: 'postgres',
+  password: 'Kalaana1@'
+});
+clientConfig.connect();
+app.get("/question",(req,res)=>
+{
+  res.json({id:0,question:'What is your age',options:[
+    {id:0,value:'20+'},
+    {id:1,value:'35+'},
+    {
+      id:2,
+      value:'45+'
+    }
+]
+});
+});
+app.get('/question/:value1',(req,res)=>
+{
+    console.log(req.params.value1);
+    data.push(req.params.value1);
+    let paramsVal=req.params.value1;
+    if(paramsVal=='20+' || paramsVal=='35+' || paramsVal=="45+")
+    {
+        res.json({id:0,question:'Select Your Gender',options:[
+        {
+           id:0,
+           value:'Male' 
+        },
+        {
+            id:1,
+            value:'Female',
+        },
+        {
+            id:2,
+            value:"lgbtqia",
+        }    
+        ]})
+    }
+    if(paramsVal=='Male' || paramsVal=='Female' || paramsVal=="lgbtqia")
+    {
+       res.json({id:0,question:'What is your prefered type of car',options:[
+         {
+             id:0,
+             value:'Hatchback',
+         },
+         {
+             id:1,
+             value:'Sedan',
+         },
+         {
+             id:3,
+             value:'SUV'
+         }
+       ]})   
+    }
+    if(paramsVal=='Hatchback')
+    {
+      res.json({id:0,question:'Choose model you prefer',options:[
+        {
+            id:0,
+            value:'Maruti Swift',
+        },
+        {
+            id:1,
+            value:'Hyundai i20',
+        },
+        {
+            id:2,
+            value:'Renault KWID',
+        },
+      ]})
+    }
+    if(paramsVal=='Sedan')
+    {
+        res.json({id:0,question:'Choose model you prefer',options:[
+            {
+                id:0,
+                value:'Hyundai Verna',
+            },
+            {
+                id:1,
+                value:'Honda City',
+            },
+            {
+                id:2,
+                value:'Volkswagen Virtus',
+            },
+          ]})
+    }
+    if(paramsVal=='SUV')
+    {
+        res.json({id:0,question:'Choose model you prefer',options:[
+            {
+                id:0,
+                value:'Mahindra Thar',
+            },
+            {
+                id:1,
+                value:'Fortuner',
+            },
+            {
+                id:2,
+                value:'XUV 700',
+            },
+          ]})
+    } 
+    if(
+        paramsVal=='Mahindra Thar' ||
+        paramsVal=='XUV 700' ||
+        paramsVal=='Fortuner' ||
+        paramsVal=='Volkswagen Virtus' ||
+        paramsVal=='Honda City' ||
+        paramsVal=='Hyundai Verna' ||
+        paramsVal=='Renault KWID' ||
+        paramsVal=='Hyundai i20' ||
+        paramsVal=='Maruti Swift'    
+)
+{
+    data.push(paramsVal);
+}
+});
+app.get("/submitdata",async(req,res)=>
+{
+  console.log(data);  
+  const createTable = await clientConfig.query('CREATE TABLE IF NOT EXISTS usersurvey(userage VARCHAR,usergender VARCHAR,usercartype VARCHAR,usercarmodel VARCHAR);');
+  const addData=await clientConfig.query('INSERT INTO usersurvey(userage,usergender,usercartype,usercarmodel) VALUES($1,$2,$3,$4)',[data[0],data[1],data[2],data[3]]);
+  console.log(addData.rows);
+  data=[];
+ console.log("success");
+ res.json({message:'success'});
+});
+app.listen(PORT,async(err)=>
+{
+    
+    if(err)console.log('server error :',err);
+    console.log('server started at port:5000');
+})
